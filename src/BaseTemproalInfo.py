1ce1c9b207a7dd508c80118b6c254765896f22f2
Qiaochu Liu <71280341+tb-liu@users.noreply.github.com>
2024-04-11 22:05:10 -0400

    Update BaseTemproalInfo.py
    successfully extract 30000 reviews with it temporal info.

--- src/BaseTemproalInfo.py
+++ src/BaseTemproalInfo.py
@@ -48,23 +48,54 @@ def split_json_file(source_file_path, output_directory, num):
         if output_file:
             output_file.close()
 
+def load_excluded_business_ids(exclude_file_path):
+    excluded_ids = set()
+    with open(exclude_file_path, 'r') as file:
+        for line in file:
+            business = json.loads(line)
+            excluded_ids.add(business['business_id'])
+    return excluded_ids
 
-def main(argv = 0, argn = 0):
+def find_reviews_with_temporal_info(source_file_path, exclude_file_path, output_file_path, max_reviews, nlp):
+    excluded_ids = load_excluded_business_ids(exclude_file_path)
+    temporal_reviews = []
+
+    with open(source_file_path, 'r') as file:
+        progress_bar = tqdm(desc="Processing reviews", unit="review", total=max_reviews)
+        for line in file:
+            review = json.loads(line.strip())
+
+            if review['business_id'] not in excluded_ids:
+                temporal_entities = extract_temporal_entities(review['text'], nlp)
+
+                if temporal_entities:
+                    review['temporal_info'] = temporal_entities
+                    temporal_reviews.append(review)
+                    progress_bar.update(1)
+
+                    if len(temporal_reviews) >= max_reviews:
+                        break
+        progress_bar.close()
+    # Convert the list of reviews to a DataFrame and save to CSV
+    df_temporal_reviews = pd.DataFrame(temporal_reviews)
+    df_temporal_reviews.to_csv(output_file_path, index=False)
+
+def main(argument = 0):
     # Load the spaCy English model
     nlp = spacy.load("en_core_web_sm")
     output_directory = './data/split_reviews'
     source_file_path = './data/yelp_academic_dataset_review.json'
-    # exclude_business = pd.read_json('./data/filtered_yelp_business.json', lines=True)
+    exclude_business = './data/filtered_yelp_business.json'
 
-    num_splits = 4
+    num_splits = 8
     # if do split data
-    if argv == 1:
+    if argument == 1:
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
-            split_json_file(source_file_path, output_directory, num_splits)
-    else: # read seperated files and process 
+        split_json_file(source_file_path, output_directory, num_splits)
+    elif argument == -1: # read seperated files and process 
         combined_df = pd.DataFrame()  # Initialize an empty for final output
-        for i in range(1, 2):  # Loop through 
+        for i in range(1, num_splits + 1):  # Loop through 
             file_path = os.path.join(output_directory, f'part_{i}.json')
             if os.path.exists(file_path):
                 review_df = pd.read_json(file_path, lines=True)
@@ -72,20 +103,25 @@ def main(argv = 0, argn = 0):
             # Process each review in the DataFrame
             tqdm_desc = f"Processing {file_path}"
             with tqdm(total=len(review_df), desc=tqdm_desc) as pbar:
-                review_df['temporal_info'] = review_df['text'].apply(lambda x: extract_temporal_entities(x, nlp))
-                pbar.update(len(review_df))
+                # Wrapper function to update progress bar after each review
+                def process_review(text):
+                    result = extract_temporal_entities(text, nlp)
+                    pbar.update(1)
+                    return result
+
+                review_df['temporal_info'] = review_df['text'].apply(process_review)
+        
 
             # Filter out reviews that have no temporal information
             processed_df = review_df[review_df['temporal_info'].map(len) > 0]
 
             combined_df = pd.concat([combined_df, processed_df])
-    # review_df = pd.read_csv('./data/sampled_yelp_reviews_with_temporal_info.csv')
-    # Sample a subset of the data, e.g., 1000 reviews
-    # sample_size = 100
-    # sampled_df = review_df.sample(n=sample_size, random_state=1)
+        # Save the combined data to a single CSV file
+        combined_df.to_csv("./data/processed_reviews_with_temporal_info.csv", index=False)
+    else:
+        max_reviews = 30000
+        find_reviews_with_temporal_info(source_file_path, exclude_business, './data/reviews_with_temporal_info.csv',max_reviews, nlp)
 
-    # Save the combined data to a single CSV file
-    combined_df.to_csv("./data/processed_reviews_with_temporal_info.csv", index=False)
 
 if __name__ == '__main__':
     main()
