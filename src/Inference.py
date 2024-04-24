cc64a18df2920be7b7994a9fc619529964a2d77f
Qiaochu Liu <71280341+tb-liu@users.noreply.github.com>
2024-04-24 03:54:34 -0400

    Update Inference.py

--- src/Inference.py
+++ src/Inference.py
@@ -1,4 +1,27 @@
 from transformers import pipeline, BertForTokenClassification, AutoTokenizer
+label_names = {'LABEL_0': 'O', 'LABEL_1': 'B-TEMP', 'LABEL_2': 'I-TEMP'}
+# Function to extract phrases from predictions
+def extract_phrases(predictions):
+    phrases = []
+    current_phrase = []
+    for prediction in predictions:
+        for token_info in prediction:
+            token = token_info['word'].replace('##', '')
+            label = label_names[token_info['entity']]
+
+            # Start a new phrase if a B-TEMP label is found
+            if label == 'B-TEMP':
+                if current_phrase:
+                    phrases.append(" ".join(current_phrase))
+                    current_phrase = []
+                current_phrase.append(token)
+            elif label == 'I-TEMP':
+                current_phrase.append(token)
+    
+    # Append the last phrase if any
+    if current_phrase:
+        phrases.append(" ".join(current_phrase))
+    return phrases
 
 # Load your trained model
 model_path = './model/temporalInfo'
@@ -13,7 +36,7 @@ text = "I visited the place last June. They open daily from 9 AM to 5 PM."
 
 # Get predictions
 predictions = nlp_pipeline(text)
-
+predictions = extract_phrases([predictions])
 # Print the predictions
 for prediction in predictions:
     print(prediction)
